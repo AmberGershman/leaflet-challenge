ac423782eafbb637ea999856d8ca92fdf90d5abd
Amber Gershman <67204846+AmberGershman@users.noreply.github.com>
2020-11-21 16:16:17 -0500

    With a lot of help from Reed, I was able to get the map to render with the geojson data layered on top

--- Leaflet-Step-1/app.js
+++ Leaflet-Step-1/app.js
@@ -1,6 +1,6 @@
-var myMap = L.map("kumquat", {
-    center: [45.52, -122.67],
-    zoom: 13
+var myMap = L.map("map", {
+  center: [41.0000, -99.1099],
+  zoom: 5
 });
 
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -12,12 +12,56 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
-function markerSize(magnitude) {
-    return magnitude * 100;
-  };
+function markerSize(mag) {
+  return (mag) * 20000
+};
 
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+function markerColor(depth) {
+  if (depth >= 30) {
+    return "#FF9F1C";
+  }
+  if (depth >= 20) {
+    return "#E71D36";
+  }
+  if (depth >= 10) {
+    return "#2EC4B6";
+  }
+  if (depth >= 5) {
+    return "#FDFFFC";
+  }
+  if (depth <= 0) {
+    return "#011627";
+  }
+  else {
+    return "#5899E2";
+  }
+};
 
-d3.json(url, function (data) {
-    console.log(data[0])
+var usgs = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+//accessing earthquake data
+d3.json(usgs, function(earthquakes){
+  for (var i =0; i <earthquakes.features.length; i++){
+      var locs = earthquakes.features[i];
+
+      if (locs && locs.properties && locs.properties.mag){
+          L.circle([locs.geometry.coordinates[1],locs.geometry.coordinates[0]],{
+              radius:markerSize(locs.properties.mag),
+              weight: 1,
+              fillColor: markerColor(locs.geometry.coordinates[2]),
+              fillOpacity: 0.8,
+          }).bindPopup("<h3>Location: " + locs.properties.place +"</h3> <hr> <h4>Mag: "+ locs.properties.mag+"<br>Depth: "+ locs.geometry.coordinates[2] + "<hr> Time: "+ new Date(locs.properties.time)+ "</h4>").addTo(myMap);
+      }
+  }
 });
+  // console.log(earthquakes.features[1].properties)
+  //   mags.push(earthquakes.features[i].properties.mag)
+  //   coords.push([earthquakes.features[i].geometry.coordinates[1], earthquakes.features[i].geometry.coordinates[0]])
+  //   var detail = earthquakes.features[i].properties.detail
+  //   L.circle([earthquakes.features[i].geometry.coordinates[1], earthquakes.features[i].geometry.coordinates[0]], {
+  //     fillOpacity: 0.75,
+  //     fillColor: markerColor(depth),
+  //     radius: markerSize(earthquakes.features[i].properties.mag),
+  //   }).bindPopup("<h2> " + earthquakes.features[i].properties.place + "</h2> <hr> <h3> Time " + new Date(earthquakes.features[i].properties.time) + "<hr> Magnitude: " + earthquakes.features[i].properties.mag + "<br> Depth: " + earthquakes.features[i].geometry[2] + "</h3>").addTo(myMap)
+  // }
+  // });
